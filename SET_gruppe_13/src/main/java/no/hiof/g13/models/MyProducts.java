a5a408ad8d5b2f9f8edf5d4e60bb08ba3489d6ca
MaRoeIT <142741541+MaRoeIT@users.noreply.github.com>
2024-09-17 12:30:09 +0200

    Added arraylist for IOTproducts in MyProducs

--- SET_gruppe_13/src/main/java/no/hiof/g13/models/MyProducts.java
+++ SET_gruppe_13/src/main/java/no/hiof/g13/models/MyProducts.java
@@ -1,4 +1,14 @@
 package no.hiof.g13.models;
 
+import java.util.ArrayList;
+
+//klasse som har listen over IOT enheter og metoder for å legge til å trekke fra enheter
 public class MyProducts {
+    //Arraylist som holder på den generelle klassen IOTDevice, Her legges IOT enhetene brukeren har til.
+    private ArrayList<IOTDevice> myProducts = new ArrayList<>();
+
+    //konstruktør
+    public MyProducts(ArrayList<IOTDevice> myProducts){
+        this.myProducts = myProducts;
+    }
 }
