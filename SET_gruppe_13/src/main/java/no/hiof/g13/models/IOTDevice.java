f4aa5dc8bdfc4b16b9b4f3eab265d8e7fefaf85a
MaRoeIT <142741541+MaRoeIT@users.noreply.github.com>
2024-09-17 12:17:48 +0200

    Added weigth to IOTDevices and comments

--- SET_gruppe_13/src/main/java/no/hiof/g13/models/IOTDevice.java
+++ SET_gruppe_13/src/main/java/no/hiof/g13/models/IOTDevice.java
@@ -1,14 +1,18 @@
 package no.hiof.g13.models;
 
+//Generell klasse som inneholder all basisinformasjon generelt om IOT enheter.
 abstract class IOTDevice {
 
     protected String name;
     protected String deviceID;
     protected String producer;
+    protected float vekt;
 
-    public IOTDevice(String name, String deviceID, String producer){
+    //konstruktør
+    public IOTDevice(String name, String deviceID, String producer, int vekt){
         this.name = name;
         this.deviceID = deviceID;
         this.producer = producer;
+        this.vekt = vekt;
     }
 }
--- SET_gruppe_13/src/main/java/no/hiof/g13/models/IOTHomeDevice.java
+++ SET_gruppe_13/src/main/java/no/hiof/g13/models/IOTHomeDevice.java
@@ -3,14 +3,17 @@ package no.hiof.g13.models;
 
 import java.util.HashMap;
 
+//Overklasse for hjemme IOT enheter, inneholder gennerelle egenskaper for IOT enheter
 public class IOTHomeDevice extends IOTDevice{
+    //Hashmap for å sette dimensioner for produktet, bredde høyde og vidde.
     protected HashMap<String, Integer> size = new HashMap<String, Integer>() {{
         put("height", 0);
         put("lenght", 0);
         put("width", 0);
     }};
-    public IOTHomeDevice(String name, String deviceID, String producer,HashMap<String, Integer> size){
-        super(name,deviceID,producer);
-        this.size = size;
+    //konstruktør
+    public IOTHomeDevice(String name, String deviceID, String producer,int vekt,HashMap<String, Integer> size){
+        super(name,deviceID,producer,vekt);
+        this.size = size; //Denne må endres på basert på tilknyttningen til databasen
     }
 }
