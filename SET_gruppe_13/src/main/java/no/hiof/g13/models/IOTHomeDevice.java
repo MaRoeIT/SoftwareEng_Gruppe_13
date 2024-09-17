431a5c203bb38c01af1429b868a774e86874b115
MaRoeIT <142741541+MaRoeIT@users.noreply.github.com>
2024-09-17 12:07:58 +0200

    Added Hashmap for dimensions
    Added hashmap for changing dimensions of the IOT device, like height, length witdth.

--- SET_gruppe_13/src/main/java/no/hiof/g13/models/IOTHomeDevice.java
+++ SET_gruppe_13/src/main/java/no/hiof/g13/models/IOTHomeDevice.java
@@ -1,4 +1,16 @@
 package no.hiof.g13.models;
 
-public class IOTHomeDevice {
+
+import java.util.HashMap;
+
+public class IOTHomeDevice extends IOTDevice{
+    protected HashMap<String, Integer> size = new HashMap<String, Integer>() {{
+        put("height", 0);
+        put("lenght", 0);
+        put("width", 0);
+    }};
+    public IOTHomeDevice(String name, String deviceID, String producer,HashMap<String, Integer> size){
+        super(name,deviceID,producer);
+        this.size = size;
+    }
 }
