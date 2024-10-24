2592d90ed1165558ee1f2b813d14dc35d299a2e7
2rawwww <143406114+2rawwww@users.noreply.github.com>
2024-10-24 23:13:03 +0200

    IOTLawnMower Finito
    Completed IOTLawnMower, tests next

--- SET_gruppe_13/src/main/java/no/hiof/g13/models/product/IOTLawnMower.java
+++ SET_gruppe_13/src/main/java/no/hiof/g13/models/product/IOTLawnMower.java
@@ -5,12 +5,19 @@ import java.util.HashMap;
 import no.hiof.g13.models.IOTHomeDevice;
 
 public class IOTLawnMower extends IOTHomeDevice {
+    public boolean isMowing;
+    public boolean omwToDock;
+    public int cuttingHeight;
+
 
     public IOTLawnMower(String name, String deviceID, String producer,
-                        String modell, boolean wifi, int weight, HashMap<String, Integer> size,
-                        int batteryLevel, int energyUsage, String lightPattern) {
-                        
-                super(name, deviceID, producer, modell, wifi, weight, size, batteryLevel);
+                        String model, boolean wifi, int weight, HashMap<String, Integer> size,
+                        int batteryLevel, int energyUsage, boolean isMowing, boolean omwToDock)
+    {
+        super(name, deviceID, producer, model, wifi, weight, size, 100);
+        this.isMowing = false;
+        this.omwToDock = false;
+        this.cuttingHeight = 50;
     }
 
     @Override
@@ -20,6 +27,49 @@ public class IOTLawnMower extends IOTHomeDevice {
     @Override
     public void updateSettings() {
         System.out.println("Settings for " + name + " has been successfully updated!");
+    }
+
+
+    public void startMowing() {
+        if (wifi && batteryLevel > 10) {
+            this.isMowing = true;
+        } else {
+            System.out.println(name + " cannot start mowing. Please check the battery level.");
+        }
+    }
+
+    public void stopMowing() {
+        if (isMowing) {
+            isMowing = false;
+            System.out.println(name + " has stopped mowing.");
+        } else {
+            System.out.println(name + " is not currently mowing.");
+        }
+    }
+
+    public void setCuttingHeight(int height) {
+        if(height>70){
+            System.out.println("\n*** Could not set the cutting height! ***\n"+ name + " only supports cutting heights between 10 and 70 mm. Please try again..\n");
+            //setCuttingHeight(height);
+        }
+        else {
+            this.cuttingHeight = height;
+            System.out.println(name + " setting cutting height: " + height + " mm.");
+            }
+        }
+
+    public void returnToDock() {
+        omwToDock = true;
+        System.out.println(name + " return to dock.");
+    }
+
+    public void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+
+    public int getBatteryLevel() {
+        System.out.println(name + ": Current battery level " + batteryLevel);
+        return batteryLevel;
     }
 
 }
