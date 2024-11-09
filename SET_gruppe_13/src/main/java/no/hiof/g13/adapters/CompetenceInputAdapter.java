97cd3e496e64a6992d0ca9af5de6fc1ade60ca5e
Kjell-Magne <kjell_m.larsen@yahoo.com>
2024-11-09 14:13:11 +0100

    Added ports, adapter and DTO for Question class. Refactored code

--- SET_gruppe_13/src/main/java/no/hiof/g13/adapters/CompetenceInputAdapter.java
+++ SET_gruppe_13/src/main/java/no/hiof/g13/adapters/CompetenceInputAdapter.java
@@ -5,19 +5,23 @@ import no.hiof.g13.ports.out.CompetenceInputPort;
 
 public class CompetenceInputAdapter implements CompetenceInputPort {
 
+    @Override
     public String getUserInput() {
         return "pending input...";
     }
 
+    @Override
     public void displayMessage(String message) {
         System.out.println(message);
     }
 
+    @Override
     public int getAnswer(Question question, String input) {
 
         return 0;
     }
 
+    @Override
     public int getUserConfig(int recommendedConfig) {
         return 0;
     }
