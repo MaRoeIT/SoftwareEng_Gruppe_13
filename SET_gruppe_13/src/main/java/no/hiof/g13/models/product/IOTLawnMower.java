d8f688c5ea250d3702811127ef7f9d939faf4d50
2rawwww <143406114+2rawwww@users.noreply.github.com>
2024-11-11 18:45:07 +0100

    Fikset kanskje feilen

--- SET_gruppe_13/src/main/java/no/hiof/g13/models/product/IOTLawnMower.java
+++ SET_gruppe_13/src/main/java/no/hiof/g13/models/product/IOTLawnMower.java
@@ -1,34 +1,33 @@
 package no.hiof.g13.models.product;
 
 import java.util.HashMap;
-
 import no.hiof.g13.models.IOTHomeDevice;
 
 public class IOTLawnMower extends IOTHomeDevice {
     public boolean isMowing;
     public boolean omwToDock;
     public int cuttingHeight;
-
+    public int energyUsage;
 
     public IOTLawnMower(String name, String deviceID, String producer,
                         String model, boolean wifi, int weight, HashMap<String, Integer> size,
-                        int batteryLevel, int energyUsage, boolean isMowing, boolean omwToDock)
-    {
-        super(name, deviceID, producer, model, wifi, weight, size, 100);
-        this.isMowing = false;
-        this.omwToDock = false;
+                        int batteryLevel, int energyUsage, boolean isMowing, boolean omwToDock) {
+        super(name, deviceID, producer, model, wifi, weight, size, batteryLevel);
+        this.isMowing = isMowing;
+        this.omwToDock = omwToDock;
         this.cuttingHeight = 50;
+        this.energyUsage = energyUsage;
     }
 
     @Override
     public void configureDevice() {
         System.out.println("Configuring " + name + " wait a minute...");
     }
+
     @Override
     public void updateSettings() {
         System.out.println("Settings for " + name + " has been successfully updated!");
     }
-
 
     public void startMowing() {
         if (wifi && batteryLevel > 10) {
@@ -48,15 +47,14 @@ public class IOTLawnMower extends IOTHomeDevice {
     }
 
     public void setCuttingHeight(int height) {
-        if(height>70){
-            System.out.println("\n*** Could not set the cutting height! ***\n"+ name + " only supports cutting heights between 10 and 70 mm. Please try again..\n");
-            //setCuttingHeight(height);
-        }
-        else {
+        if (height > 70) {
+            System.out.println("\n*** Could not set the cutting height! ***\n" + name
+                    + " only supports cutting heights between 10 and 70 mm. Please try again..\n");
+        } else {
             this.cuttingHeight = height;
             System.out.println(name + " setting cutting height: " + height + " mm.");
-            }
         }
+    }
 
     public void returnToDock() {
         omwToDock = true;
@@ -71,5 +69,4 @@ public class IOTLawnMower extends IOTHomeDevice {
         System.out.println(name + ": Current battery level " + batteryLevel);
         return batteryLevel;
     }
-
 }
