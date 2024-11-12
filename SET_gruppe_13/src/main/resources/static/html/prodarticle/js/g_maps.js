0bf872807832780dcae06e5dd2d01dd2e8c2604e
AndreasIsaksen <andreis@hiof.no>
2024-11-12 20:07:34 +0100

    katalog/kontakt
    Har laget en katalog for produkter, samt en kontaktside for selskapet.

---
+++ SET_gruppe_13/src/main/resources/static/html/prodarticle/js/g_maps.js
@@ -0,0 +1,7 @@
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(59.129319039336025, 11.352760382206785),
+      zoom:15,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
